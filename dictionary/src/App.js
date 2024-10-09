88efd772d220111cfa070ea9b9a37cf6d2e99e47
HayaTamimi <145218954+HayaTamimi@users.noreply.github.com>
2024-10-09 21:30:59 +0300

    added axios

--- dictionary/src/App.js
+++ dictionary/src/App.js
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className='container'>
+        <h1>React Dictionary ✨</h1>
         <Dictionary />
       </div>
     </div>
--- dictionary/src/Dictionary.js
+++ dictionary/src/Dictionary.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("null");
+  function handleResponse(response) {
+    
+  }
   function search(event) {
     event.preventDefault();
-    alert("searchin...");
+
+    // documentation https://dictionaryapi.dev/
+    
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
     function handleKeywordChange(event) {
-        console.log(event);
+        setKeyword(event.target.value);
   }
   return (
     <div className="Dictionary">
